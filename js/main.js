286353a3da6f5e12bc047921b691ee9526afb864
Christoffer Wallenberg <christoffer.wallenberg@teliasonera.com>
2015-03-22 15:49:40 +0100

    Removed show/hide of circle

--- js/main.js
+++ js/main.js
@@ -8,17 +8,6 @@ $(document).ready(function () {
 		$('.overlay').css('display', 'none');
 	});
 
-	var circle = $('.circle');
-
-	$(window).scroll(function () {
-		var circlePos = circle.offset().top;
-		if (circlePos > 600) {
-			circle.css('display', 'block');
-		} else {
-			circle.css('display', 'none');
-		}
-	});
-
 	$('.circle').click(function () {
 		$('body, html').animate({
 			scrollTop: $('body, html').offset().top
